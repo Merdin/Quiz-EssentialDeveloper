f908e8f3e6808a0eef629c3805868e7c2004d49b
Merdin <merdin10@icloud.com>
2023-02-13 18:03:35 +0100

    Remove Question Hash custom implementation

--- QuizEngine/QuizEngine/Question.swift
+++ QuizEngine/QuizEngine/Question.swift
@@ -10,13 +10,4 @@ import Foundation
 public enum Question<T: Hashable>: Hashable {
     case singleAnswer(T)
     case multipleAnswer(T)
-    
-    public func hash(into hasher: inout Hasher) {
-        switch self {
-        case .singleAnswer(let value):
-            hasher.combine("singleAnswer \(value)")
-        case .multipleAnswer(let value):
-            hasher.combine("multipleAnswer \(value)")
-        }
-    }
 }
