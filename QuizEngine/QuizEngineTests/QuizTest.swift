0ba8408d35d634335e8d90a3dcb4f29ab34db215
Merdin <merdin10@icloud.com>
2023-02-13 23:12:48 +0100

    Clarified test name

--- QuizEngine/QuizEngineTests/QuizTest.swift
+++ QuizEngine/QuizEngineTests/QuizTest.swift
@@ -13,7 +13,7 @@ class QuizTest: XCTestCase {
     
     private var quiz: Quiz!
     
-    func test_startQuiz_answerZeroOutOfTwoCorrectly_scoresZero() {
+    func test_startQuiz_answersAllquestions_completesWithAnswers() {
         let delegate = DelegateSpy()
         
         quiz = Quiz.start(questions: ["Q1", "Q2"], delegate: delegate)
