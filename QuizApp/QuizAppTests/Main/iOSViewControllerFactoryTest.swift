be89bf0694971b3dfd134bdd50702915c5192058
Merdin <merdin10@icloud.com>
2023-02-14 16:57:39 +0100

    Updated the test factory method to use the new ResultsPresenter initialiser

--- QuizApp/QuizAppTests/Main/iOSViewControllerFactoryTest.swift
+++ QuizApp/QuizAppTests/Main/iOSViewControllerFactoryTest.swift
@@ -75,35 +75,32 @@ class iOSViewControllerFactoryTest: XCTestCase {
         return iOSViewControllerFactory(questions: [singleAnswerQuestion, multipleAnswerQuestion], options: options, correctAnswers: correctAnswers)
     }
     
+    func makeSUT(options: Dictionary<Question<String>, [String]> = [:], correctAnswers: [(question: Question<String>, answers: [String])] = []) -> iOSViewControllerFactory {
+        return iOSViewControllerFactory(options: options, correctAnswers: correctAnswers)
+    }
+    
     func makeQuestionController(question: Question<String> = Question.singleAnswer("")) -> QuestionViewController {
-        return makeSUT(options: [question: options]).questionViewController(for: question) { _ in } as! QuestionViewController
+        return makeSUT(options: [question: options], correctAnswers: [:]).questionViewController(for: question) { _ in } as! QuestionViewController
     }
     
     func makeResults() -> (controller: ResultsViewController, presenter: ResultsPresenter) {
-        let userAnswers = [singleAnswerQuestion: ["A1"], multipleAnswerQuestion: ["A1", "A2"]]
-        let correctAnswers = [singleAnswerQuestion: ["A1"], multipleAnswerQuestion: ["A1", "A2"]]
-        let questions = [singleAnswerQuestion, multipleAnswerQuestion]
+        let userAnswers = [(singleAnswerQuestion, ["A1"]), (multipleAnswerQuestion, ["A1", "A2"])]
+        let correctAnswers = [(singleAnswerQuestion, ["A1"]), (multipleAnswerQuestion, ["A1", "A2"])]
         
-        let result = Result(answers: userAnswers, score: 2)
+        let result = Result(
+            answers: [singleAnswerQuestion: ["A1"], multipleAnswerQuestion: ["A1", "A2"]],
+            score: 2
+        )
 
-        let presenter = ResultsPresenter(result: result, questions: questions, correctAnswers: correctAnswers)
+        let presenter = ResultsPresenter(
+            userAnswers: userAnswers,
+            correctAnswers: correctAnswers,
+            scorer: { _, _ in result.score }
+        )
         let sut = makeSUT(correctAnswers: correctAnswers)
         
         let controller = sut.resultViewController(for: result) as! ResultsViewController
         
         return (controller, presenter)
-    }
-}
-
-private extension ResultsPresenter {
-    convenience init(result: Result<Question<String>, [String]>, questions: [Question<String>], correctAnswers: Dictionary<Question<String>, [String]>) {
-        self.init(
-            userAnswers: questions.map { question in
-                (question, result.answers[question]!)
-            },
-            correctAnswers: questions.map { question in
-                (question, correctAnswers[question]!)
-            },
-            scorer: { _, _ in result.score })
     }
 }
